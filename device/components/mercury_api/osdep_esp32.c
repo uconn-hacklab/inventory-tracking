460d65f6139608944a3018b802e0dc46ffdb7b9f
kalinkochnev <kalin.kochnev@gmail.com>
2024-08-13 20:30:24 -0400

    Added system time functionality. Follows POSIX standard.

--- device/components/mercury_api/osdep_esp32.c
+++ device/components/mercury_api/osdep_esp32.c
@@ -5,16 +5,31 @@
  *  @date 8/13/2024
  */
 
+#include "freertos/FreeRTOS.h"
+#include <sys/time.h>   // For gettimeofday() and struct timeval
+#include <time.h>
 #include <stdint.h>
 #include "osdep.h"
 
+uint64_t
+tmr_gettime()
+{
+  struct timeval tv;
+  uint64_t totalms;
+
+  gettimeofday(&tv, NULL);
+  totalms = (((uint64_t)tv.tv_sec) * 1000) + ((uint64_t) tv.tv_usec) / 1000;
+
+  return totalms;
+}
 
 uint32_t tmr_gettime_low()
 {
   /* Fill in with code that returns the low 32 bits of a millisecond
    * counter. The API will not otherwise interpret the counter value.
    */
-  return 0;
+
+  return (tmr_gettime() >>  0) & 0xffffffff;
 }
 
 uint32_t tmr_gettime_high()
@@ -23,7 +38,8 @@ uint32_t tmr_gettime_high()
    * counter. The API will not otherwise interpret the counter value.
    * Returning 0 is acceptable here if you do not have a large enough counter.
    */
-  return 0;
+
+  return (tmr_gettime() >> 32) & 0xffffffff;
 }
 
 void
@@ -33,19 +49,26 @@ tmr_sleep(uint32_t sleepms)
    * Fill in with code that returns after at least sleepms milliseconds
    * have elapsed.
    */
+  vTaskDelay(sleepms / portTICK_PERIOD_MS);
 }
 
 TMR_TimeStructure
 tmr_gettimestructure()
 {
+  time_t now;
+  uint64_t temp;
+  struct tm *timestamp;
   TMR_TimeStructure timestructure;
 
-  timestructure.tm_year = (uint32_t)0;
-  timestructure.tm_mon = (uint32_t)0;
-  timestructure.tm_mday = (uint32_t)0;
-  timestructure.tm_hour = (uint32_t)0;
-  timestructure.tm_min = (uint32_t)0;
-  timestructure.tm_sec = (uint32_t)0;
+  temp = tmr_gettime();
+  now = (time_t)(temp/1000);
+  timestamp = localtime(&now);
 
-  return timestructure;
+  timestructure.tm_year = (uint32_t)(1900 + timestamp->tm_year);
+  timestructure.tm_mon = (uint32_t)(1 + timestamp->tm_mon);
+  timestructure.tm_mday = (uint32_t)timestamp->tm_mday;
+  timestructure.tm_hour = (uint32_t)timestamp->tm_hour;
+  timestructure.tm_min = (uint32_t)timestamp->tm_min;
+  timestructure.tm_sec = (uint32_t)timestamp->tm_sec;
+  return timestructure;  
 }
